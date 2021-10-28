a75ad09938d696b0299451a79ae17f5c30f038a4
Felipe Santos <felipemillhouse@gmail.com>
2021-10-28 13:20:15 -0400

    instal expo modules

--- android/app/src/main/java/com/aquamind/MainActivity.java
+++ android/app/src/main/java/com/aquamind/MainActivity.java
@@ -1,4 +1,6 @@
 package com.aquamind;
+import expo.modules.ReactActivityDelegateWrapper;
+import com.facebook.react.ReactActivityDelegate;
 
 import com.facebook.react.ReactActivity;
 
@@ -11,5 +13,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Aquamind";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegateWrapper(this,
+      new ReactActivityDelegate(this, getMainComponentName())
+    );
   }
 }
