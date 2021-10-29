5deb62ad05939b32d89aab285636b6eff2a064a9
Felipe Santos <felipemillhouse@gmail.com>
2021-10-29 12:27:25 -0400

    set refreshtoken on axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,71 @@
 import axios from 'axios'
+import * as SecureStore from 'expo-secure-store'
 
-export default axios.create({
-  baseURL: 'http://127.0.0.1:3000',
+const baseURL = 'http://127.0.0.1:3000'
+const api = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
+
+api.interceptors.request.use(
+  async config => {
+    const token = await SecureStore.getItemAsync('accessToken')
+    if (token) {
+      config.headers!.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  },
+)
+
+api.interceptors.response.use(
+  res => {
+    return res
+  },
+  async err => {
+    const originalConfig = err.config
+
+    if (err.response) {
+      if (err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true
+
+        const token = await SecureStore.getItemAsync('refreshToken')
+        const refreshAccessTokenAPI = axios.create({
+          baseURL,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        return refreshAccessTokenAPI
+          .get('/auth/refreshAccessToken', {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then(async refreshTokenResponse => {
+            const { accessToken } = refreshTokenResponse.data
+            await SecureStore.setItemAsync('accessToken', accessToken)
+            api.defaults.headers.common.Authorization = `Bearer ${accessToken}`
+
+            return api(originalConfig)
+          })
+          .catch(_error => {
+            if (_error.response && _error.response.data) {
+              return Promise.reject(_error.response.data)
+            }
+
+            return Promise.reject(_error)
+          })
+      }
+    }
+
+    return Promise.reject(err)
+  },
+)
+
+export default api
