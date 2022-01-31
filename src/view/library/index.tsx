19735d81a044ca0e0583cf2fdc382f66e0ac36b0
Felipe Santos <felipemillhouse@gmail.com>
2022-01-31 17:36:39 -0500

    library screen

--- src/view/library/index.tsx
+++ src/view/library/index.tsx
@@ -1,12 +1,48 @@
 import React from 'react'
 
 import { NavPropsLib } from '../../routes/types'
-import { Container, PaperText } from './styles'
+import {
+  Container,
+  PaperRedText,
+  PaperContainer,
+  RowView,
+  Image,
+  PaperTitle,
+} from './styles'
 
 const Library: React.FC<NavPropsLib> = () => {
   return (
     <Container>
-      <PaperText>Library</PaperText>
+      <RowView>
+        <PaperContainer>
+          <Image source={require('../../assets/icons/libPlants.png')} />
+          <PaperTitle>Plants</PaperTitle>
+        </PaperContainer>
+        <PaperContainer>
+          <Image source={require('../../assets/icons/root.png')} />
+          <PaperTitle>Wood</PaperTitle>
+          <PaperRedText>coming soon</PaperRedText>
+        </PaperContainer>
+      </RowView>
+      <RowView>
+        <PaperContainer>
+          <Image source={require('../../assets/icons/stone.png')} />
+          <PaperTitle>Rocks</PaperTitle>
+          <PaperRedText>coming soon</PaperRedText>
+        </PaperContainer>
+        <PaperContainer>
+          <Image source={require('../../assets/icons/algae.png')} />
+          <PaperTitle>Algae</PaperTitle>
+          <PaperRedText>coming soon</PaperRedText>
+        </PaperContainer>
+      </RowView>
+      <RowView>
+        <PaperContainer>
+          <Image source={require('../../assets/Avatar.png')} />
+          <PaperTitle>Fish</PaperTitle>
+          <PaperRedText>coming soon</PaperRedText>
+        </PaperContainer>
+      </RowView>
     </Container>
   )
 }
--- src/view/library/styles.ts
+++ src/view/library/styles.ts
@@ -1,10 +1,28 @@
 import styled from 'styled-components/native'
-import { Text } from 'react-native-paper'
+import { Text, Surface, Title } from 'react-native-paper'
 
 import theme from '../../theme'
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${theme.colors.background};
+  background-color: ${theme.colors.darkenBackground};
 `
-export const PaperText = styled(Text)``
+export const PaperRedText = styled(Text)`
+  color: ${theme.colors.error};
+`
+export const PaperTitle = styled(Title)``
+export const RowView = styled.View`
+  flex-direction: row;
+`
+export const Image = styled.Image`
+  width: ${theme.sizes.width / 5}px;
+  height: ${theme.sizes.width / 5}px;
+`
+export const PaperContainer = styled(Surface)`
+  margin: ${theme.sizes.margin}px;
+  padding: ${theme.sizes.padding}px;
+  flex: 1;
+  border-radius: ${theme.roundness}px;
+  align-items: center;
+  justify-content: center;
+`
