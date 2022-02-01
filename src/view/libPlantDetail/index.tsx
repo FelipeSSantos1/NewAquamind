4602b031130808ec4af55d7273b09348a43ae0d7
Felipe Santos <felipemillhouse@gmail.com>
2022-02-01 11:48:35 -0500

    implement loading on plant detail screen

--- src/view/libPlantDetail/index.tsx
+++ src/view/libPlantDetail/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { NavPropsLibPlantDetail } from '../../routes/types'
+import Loading from '../components/fakeLoadingScreen'
 import * as PlantAPI from '../../API/plant'
 import { PlantDetail } from '../../API/plant/types'
 import {
@@ -13,18 +14,28 @@ import {
 } from './styles'
 import { fullImageUrl } from '../../services/helper'
 
-const LibPlantDetail: React.FC<NavPropsLibPlantDetail> = ({ route }) => {
+const LibPlantDetail: React.FC<NavPropsLibPlantDetail> = ({
+  navigation,
+  route,
+}) => {
+  const [ready, setReady] = React.useState(false)
   const [selectedPlant, setSelectedPlant] = React.useState<PlantDetail>()
   React.useEffect(() => {
     const fetchPlant = async () => {
       const response = await PlantAPI.getById(route.params.plantId)
       if (!response || 'statusCode' in response) {
+        navigation.pop()
         return
       }
       setSelectedPlant(response)
+      setReady(true)
     }
     fetchPlant()
-  }, [route.params.plantId])
+  }, [navigation, route.params.plantId])
+
+  if (!ready) {
+    return <Loading />
+  }
 
   return (
     <Container showsVerticalScrollIndicator={false}>
