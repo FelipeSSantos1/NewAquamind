46209ce2071196c88f44fa40ab32d8a28976c5c3
Felipe Santos <felipemillhouse@gmail.com>
2022-02-01 09:24:23 -0500

    plant list screen

--- src/view/libPlantsList/index.tsx
+++ src/view/libPlantsList/index.tsx
@@ -1,13 +1,72 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { TouchableRipple, Divider } from 'react-native-paper'
+import { useSelector, useDispatch } from 'react-redux'
+import { FlatList } from 'react-native'
+import filter from 'lodash/filter'
+import toUpper from 'lodash/toUpper'
 
+import * as API from '../../API/plant'
+import PlantRTK from '../../store/plant'
+import { PlantState } from '../../store/plant/types'
 import { NavPropsLibPlants } from '../../routes/types'
-import { Container, PaperText } from './styles'
+import { RootState } from '../../store/rootReducer'
+import { fullImageUrl } from '../../services/helper'
+import { GoDetailProps } from './types'
+import { MainView, Searchbar, Text, RowView, ThumbImage } from './styles'
 
-const LibPlantsList: React.FC<NavPropsLibPlants> = () => {
+const LibPlantsList: React.FC<NavPropsLibPlants> = ({ navigation }) => {
+  const dispatch = useDispatch()
+  const [search, setSearch] = useState('')
+  const { plant } = useSelector((state: RootState) => state)
+
+  useEffect(() => {
+    const fetchPlants = async () => {
+      const response = await API.getAll()
+      if (!response || 'statusCode' in response) {
+        return
+      }
+      dispatch(PlantRTK.actions.setPlant(response))
+    }
+    fetchPlants()
+  }, [dispatch])
+
+  const changeSearchText = (text: string) => {
+    setSearch(text)
+  }
+
+  const goDetail = async ({ id }: GoDetailProps) => {
+    navigation.navigate('LibPlantDetail', {
+      plantId: id,
+    })
+  }
+
+  const renderList = (item: PlantState) => (
+    <TouchableRipple onPress={() => goDetail({ id: item.id })}>
+      <RowView>
+        <ThumbImage source={fullImageUrl(item.avatar)} resizeMode="contain" />
+        <Text>{item.name}</Text>
+      </RowView>
+    </TouchableRipple>
+  )
+
   return (
-    <Container>
-      <PaperText>Plant List</PaperText>
-    </Container>
+    <MainView>
+      <Searchbar
+        value={search}
+        onChangeText={text => changeSearchText(text)}
+        autoComplete="off"
+      />
+      <FlatList
+        data={filter(plant, item => {
+          if (toUpper(item.name).search(toUpper(search)) !== -1) {
+            return true
+          }
+          return false
+        })}
+        renderItem={({ item }) => renderList(item)}
+        ItemSeparatorComponent={() => <Divider />}
+      />
+    </MainView>
   )
 }
 
--- src/view/libPlantsList/styles.ts
+++ src/view/libPlantsList/styles.ts
@@ -1,10 +1,31 @@
 import styled from 'styled-components/native'
-import { Text } from 'react-native-paper'
+import FastImage from 'react-native-fast-image'
+import {
+  Searchbar as PaperSearchbar,
+  Text as PaperText,
+} from 'react-native-paper'
 
 import theme from '../../theme'
 
-export const Container = styled.View`
+export const MainView = styled.View`
   flex: 1;
-  background-color: ${theme.colors.background};
+  background-color: ${theme.colors.surface};
+  padding: ${theme.sizes.pagePadding}px;
 `
-export const PaperText = styled(Text)``
+export const Searchbar = styled(PaperSearchbar)`
+  margin-bottom: ${theme.sizes.margin}px;
+`
+export const Text = styled(PaperText)`
+  margin: ${theme.sizes.margin}px 0;
+`
+export const RowView = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+export const ThumbImage = styled(FastImage)`
+  width: ${50}px;
+  height: ${50}px;
+  border-radius: ${theme.roundness}px;
+  align-self: flex-start;
+  margin-right: ${theme.sizes.margin}px;
+`
