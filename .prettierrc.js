7498b7de6951c202f7a6dff9306b71c4dc256823
Felipe Santos <felipemillhouse@gmail.com>
2022-03-09 12:41:45 -0500

    change lint

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: true,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  arrowParens: 'avoid',
-  endOfLine: 'lf',
-  semi: false,
-  jsxBracketSameLine: false
-};
